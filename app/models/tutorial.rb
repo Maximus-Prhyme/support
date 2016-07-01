ee3d1b6a60b40e80638304b37e7463afbb53f4bd
Kevin Ridge <mrrydge91@gmail.com>
2016-06-30 23:18:58 -0400

    good enough?

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,6 +1,6 @@
 class Tutorial < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :picture, styles: { large: "900x900", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :picture, styles: { large: "1200x1200", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :picture,
                                     :content_type => /^image\/(png|gif|jpeg)/,
                                     :message => 'only (png/gif/jpeg) images'
