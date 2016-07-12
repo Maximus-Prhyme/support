ae43c8d1e251ae62bbd886eac1b8ac4e3081dbb3
Kevin Ridge <mrrydge91@gmail.com>
2016-07-12 16:14:47 -0400

    resize stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
 CarrierWave.configure do |config|                       # required
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'add access key here',                        # required
-    :aws_secret_access_key  => 'secret access key'                      # required
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],
+    :region                 => ENV['S3_REGION']     # required
   }
   config.fog_directory  = 'biotrack'                          # required
 end
