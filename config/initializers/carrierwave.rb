bec2fcd48ccb6e3f608a65f795752ef50a354e54
Kevin Ridge <mrrydge91@gmail.com>
2016-07-19 15:07:54 -0400

    caption recolor

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|                       # required
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
+    :aws_access_key_id     => 'AKIAIGR66DIUTX7VGMAA',
+    :aws_secret_access_key => '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'
+    # :region                => ENV['S3_REGION']
                      # required
   }
   config.fog_directory  = 'biotrack'                          # required
